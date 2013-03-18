53f39dc642daea16b28c85ad0c875b297fc3eb07
Hans-Gunther Schmidt <hans@otype.de>
2013-03-18 21:37:53 +0100

    adding new exception types RiakObjectNotFoundException, RiakObjectDuplicateFoundException, RiakObjectIdNotProvidedException

--- pybuildr/pybuildr/exceptions.py
+++ pybuildr/pybuildr/exceptions.py
@@ -19,3 +19,38 @@ class NoDictionaryException(BaseException):
         if message:
             error_message = message
         super(NoDictionaryException, self).__init__(error_message, *args, **kwargs)
+
+class RiakObjectNotFoundException(BaseException):
+    """
+        Thrown when a received message is of unknown type
+    """
+
+    def __init__(self, message=None, *args, **kwargs):
+        error_message = 'Object with given id not found!'
+        if message:
+            error_message = message
+        super(RiakObjectNotFoundException, self).__init__(error_message, *args, **kwargs)
+
+class RiakObjectDuplicateFoundException(BaseException):
+    """
+        Thrown when a received message is of unknown type
+    """
+
+    def __init__(self, message=None, *args, **kwargs):
+        error_message = 'Object with given id already exists!'
+        if message:
+            error_message = message
+        super(RiakObjectDuplicateFoundException, self).__init__(error_message, *args, **kwargs)
+
+
+class RiakObjectIdNotProvidedException(BaseException):
+    """
+        Thrown when an object ID was required but not provided
+    """
+
+    def __init__(self, message=None, *args, **kwargs):
+        error_message = 'No object ID provided! Object ID required.'
+        if message:
+            error_message = message
+
+        super(RiakObjectIdNotProvidedException, self).__init__(error_message, *args, **kwargs)

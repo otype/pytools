36f13f783aebbcc0ed2bbc509f90149d0b3283d8
Hans-Gunther Schmidt <hans@otype.de>
2013-03-19 01:14:13 +0100

    all exceptions are now raising HTTPError from tornado with corresponding HTTP Error codes

--- pybuildr/pybuildr/exceptions.py
+++ pybuildr/pybuildr/exceptions.py
@@ -8,6 +8,7 @@
     Copyright (c) 2012 apitrary
 
 """
+from tornado.web import HTTPError
 
 class NoDictionaryException(BaseException):
     """
@@ -15,10 +16,12 @@ class NoDictionaryException(BaseException):
     """
 
     def __init__(self, message=None, *args, **kwargs):
-        error_message = 'No dictionary provided!'
+        error_message = 'No dictionary provided'
         if message:
             error_message = message
         super(NoDictionaryException, self).__init__(error_message, *args, **kwargs)
+        raise HTTPError(status_code=500, log_message=error_message)
+
 
 class RiakObjectNotFoundException(BaseException):
     """
@@ -26,10 +29,12 @@ class RiakObjectNotFoundException(BaseException):
     """
 
     def __init__(self, message=None, *args, **kwargs):
-        error_message = 'Object with given id not found!'
+        error_message = 'Object with given id not found'
         if message:
             error_message = message
         super(RiakObjectNotFoundException, self).__init__(error_message, *args, **kwargs)
+        raise HTTPError(status_code=404, log_message=error_message)
+
 
 class RiakObjectDuplicateFoundException(BaseException):
     """
@@ -37,10 +42,11 @@ class RiakObjectDuplicateFoundException(BaseException):
     """
 
     def __init__(self, message=None, *args, **kwargs):
-        error_message = 'Object with given id already exists!'
+        error_message = 'Object with given id already exists. Content not modified.'
         if message:
             error_message = message
         super(RiakObjectDuplicateFoundException, self).__init__(error_message, *args, **kwargs)
+        raise HTTPError(status_code=304, log_message=error_message)
 
 
 class RiakObjectIdNotProvidedException(BaseException):
@@ -49,8 +55,8 @@ class RiakObjectIdNotProvidedException(BaseException):
     """
 
     def __init__(self, message=None, *args, **kwargs):
-        error_message = 'No object ID provided! Object ID required.'
+        error_message = 'No object ID provided. Object ID required.'
         if message:
             error_message = message
-
         super(RiakObjectIdNotProvidedException, self).__init__(error_message, *args, **kwargs)
+        raise HTTPError(status_code=400, log_message=error_message)

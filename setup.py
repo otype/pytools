1a33ea4525c5c45cbd372bd5c4064632900e65b4
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-21 00:07:18 +0200

    version v0.1.2

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def scripts_list():
 
 setup(
     name='pytools',
-    version='0.1.1',
+    version='0.1.2',
     author='Hans-Gunther Schmidt',
     author_email='hgs@apitrary.com',
     description='pytools - apitrary pytools',

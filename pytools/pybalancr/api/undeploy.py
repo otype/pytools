6006fc8d31f904c30bd34e6a5b67d1ee0907b7fd
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-21 00:31:07 +0200

    playing around with .to_dict()

--- pytools/pybalancr/api/undeploy.py
+++ pytools/pybalancr/api/undeploy.py
@@ -17,11 +17,11 @@ DEPLOY_QUEUE = 'balancr.undeploy'
 
 
 def loadbalance_undeploy(api_id):
-    loadbalance_remove__message = LoadbalanceRemoveMessage(api_id=api_id)
-    logging.debug("Received Loadbalance API remove message: {}".format(loadbalance_remove__message.to_json()))
+    loadbalance_remove_message = LoadbalanceRemoveMessage(api_id=api_id)
+    logging.debug("Received Loadbalance API remove message: {}".format(loadbalance_remove_message.to_json()))
 
     return balancr_tasks.undeploy.apply_async(
-        args=[loadbalance_remove__message.to_dict()],
+        args=[loadbalance_remove_message.to_dict()],
         queue=DEPLOY_QUEUE,
         routing_key=DEPLOY_ROUTING_KEY
     ).get()

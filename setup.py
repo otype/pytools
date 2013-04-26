0743359a918b07dacdf7289e5ac87ec971e3d8d9
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-26 17:51:40 +0200

    bumping up version to 0.1.10

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def scripts_list():
 
 setup(
     name='pytools',
-    version='0.1.9',
+    version='0.1.10',
     author='Hans-Gunther Schmidt',
     author_email='hgs@apitrary.com',
     description='pytools - apitrary pytools',

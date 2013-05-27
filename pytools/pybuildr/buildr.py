f6dfd0e893f0c9167b2c315c307e9effd12aec7a
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-05-27 23:00:21 +0200

    bumping up version to 0.2.0

--- pytools/pybuildr/buildr.py
+++ pytools/pybuildr/buildr.py
@@ -26,7 +26,7 @@ enable_pretty_logging()
 
 APP_DETAILS = {
     'name': 'pybuildr',
-    'version': '0.1',
+    'version': '0.2.0',
     'company': 'apitrary',
     'author': 'Hans-Gunther Schmidt',
     'author_email': 'hgs@apitrary.com',
